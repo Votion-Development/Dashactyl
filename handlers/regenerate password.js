9f9fb6829efbafdfd75c6e3a5c23b595f7743a51
Two <njsolomonoh@gmail.com>
2022-02-09 10:59:42 -0500

    Update regenerate password.js

--- handlers/regenerate password.js
+++ handlers/regenerate password.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch')
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const functions = require('../functions.js')
 
 module.exports.load = async function (app, ifValidAPI, ejs) {

49dc934f010dcf9d36fa9e97f57107afd5f54d72
devnote-dev <inferno0745@gmail.com>
2022-05-03 01:28:35 +0100

    feat(routers): implement endpoint & types

--- routers/auth.ts
+++ routers/auth.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from 'fastify';
 import type Logger from '../log';
 import AccountManager from '../managers/accounts';
-import { Closure } from '.';
+import Permissions from '../managers/permissions';
+import { Closure, SessionType } from '.';
 
 export default async function (log: Logger, ctx: FastifyInstance, done: Closure) {
     ctx.addHook('onError', (_, res, err) => {
@@ -33,13 +34,49 @@ export default async function (log: Logger, ctx: FastifyInstance, done: Closure)
         req.session.account = {
             user,
             servers: [],
-            type: 1,
-            isAdmin: true
+            type: SessionType.RETURNING,
+            isAdmin: false
         };
         return res.redirect('/dashboard');
     });
 
-    ctx.post('/signup', async (req, res) => {});
+    ctx.post('/signup', {
+        schema:{
+            body:{
+                type: 'object',
+                required:['username', 'email', 'password'],
+                properties:{
+                    username:{ type: 'string' },
+                    email:{ type: 'string' },
+                    password:{ type: 'string' }
+                }
+            }
+        }
+    }, async (req, res) => {
+        const body = req.body as Record<string, string>;
+        let user = await AccountManager.get(body.email);
+        if (user) return res.redirect('/signup?err=ACCOUNTEXISTS');
+        user = await AccountManager.create({
+            username: body.username,
+            email: body.email,
+            password: body.password,
+            avatar: 'https://www.gravatar.com/0',
+            package: 'default',
+            referral: null,
+            permissions: Permissions.getUser(),
+            suspended: false,
+            createdAt: Date.now(),
+            lastLogin: Date.now()
+        });
+
+        req.session.account = {
+            user,
+            servers: [],
+            type: SessionType.NEW_ACCOUNT,
+            isAdmin: false
+        }
+        return res.redirect('/dashboard');
+    });
 
     done();
 }
--- routers/index.ts
+++ routers/index.ts
@@ -1,10 +1,22 @@
 import { FastifyInstance } from 'fastify';
 import type Logger from '../log';
+import { IAccount } from '../models/account';
 import authRouter from './auth';
 
 export type Closure = (err?: Error | undefined) => void;
 
-export interface Context {}
+export interface Context {
+    user:       IAccount;
+    servers:    unknown[];
+    type:       SessionType;
+    isAdmin:    boolean;
+}
+
+export enum SessionType {
+    NONE,
+    RETURNING,
+    NEW_ACCOUNT
+}
 
 export default async function (log: Logger, ctx: FastifyInstance, done: Closure) {
     ctx.setNotFoundHandler((_, res) => res.view('errors.ejs', {
@@ -38,7 +50,7 @@ export default async function (log: Logger, ctx: FastifyInstance, done: Closure)
     });
 
     ctx.get('/', (_, res) => {
-        res.view<Context>('home.ejs', {});
+        res.view('home.ejs');
     });
 
     done();

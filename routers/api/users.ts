266ae9c4a92a21adfaa520f3f6b6f179c84e97f8
devnote-dev <inferno0745@gmail.com>
2022-05-05 02:38:25 +0100

    fix(api): use separate method for id fetch

--- routers/api/users.ts
+++ routers/api/users.ts
@@ -8,35 +8,28 @@ export default async function (
     ctx: FastifyInstance,
     done: Closure
 ): Promise<void> {
-    ctx.get('/users', {
-        schema:{
-            params:{
-                type: 'object',
-                properties:{
-                    id:{ type: 'string' }
-                }
-            }
-        }
-    }, async (req, res) => {
-        const params = req.params as Record<string, string>;
-        if (params.id) {
-            const user = await AccountManager.get(params.id);
-            if (!user) return res.status(404).send({
-                status: 'error',
-                data:{
-                    code: 404,
-                    error: 'user account with that id not found'
-                }
-            });
-            return res.send({
-                status: 'ok',
-                data: transform(user)
-            });
-        }
+    ctx.get('/users', async (_, res) => {
         const users = await AccountManager.fetch();
         return res.send({
             status: 'ok',
             data: users.map(transform)
+        });
+    });
+
+    ctx.get('/users/:id', async (req, res) => {
+        const params = req.params as Record<string, string>;
+        const user = await AccountManager.get(params.id);
+        if (!user) return res.status(404).send({
+            status: 'error',
+            data:{
+                code: 404,
+                error: 'user account with that id not found'
+            }
+        });
+
+        return res.send({
+            status: 'ok',
+            data: transform(user)
         });
     });
 

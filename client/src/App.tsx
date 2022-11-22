8c182632df89d51d7110b308016224330eea46d3
devnote-dev <inferno0745@gmail.com>
2022-11-22 21:37:19 +0000

    feat(client): add tailwind support

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ client/tailwind.config.ts
@@ -0,0 +1,9 @@
+import { Config } from 'tailwindcss';
+
+export default <Config>{
+  content: ['src/**/*.tsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

c2027be130868919c05a46b78244688f028d8b3e
devnote-dev <inferno0745@gmail.com>
2022-11-26 01:46:30 +0000

    feat(prisma): update database tables

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,39 +4,14 @@ import bcrypt from "bcryptjs";
 const prisma = new PrismaClient();
 
 async function seed() {
-  const email = "rachel@remix.run";
+  const email = "dashactyl@votion.dev";
 
-  // cleanup the existing database
-  await prisma.user.delete({ where: { email } }).catch(() => {
-    // no worries if it doesn't exist yet
-  });
-
-  const hashedPassword = await bcrypt.hash("racheliscool", 10);
-
-  const user = await prisma.user.create({
+  await prisma.user.delete({ where: { email } }).catch(() => {});
+  await prisma.user.create({
     data: {
+      username: 'Dashactyl',
       email,
-      password: {
-        create: {
-          hash: hashedPassword,
-        },
-      },
-    },
-  });
-
-  await prisma.note.create({
-    data: {
-      title: "My first note",
-      body: "Hello, world!",
-      userId: user.id,
-    },
-  });
-
-  await prisma.note.create({
-    data: {
-      title: "My second note",
-      body: "Hello, world!",
-      userId: user.id,
+      password: await bcrypt.hash('dashactylv3', 10)
     },
   });
 

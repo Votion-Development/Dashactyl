051922bc31347417b3c85eea58d0428cf35a91fc
devnote-dev <inferno0745@gmail.com>
2022-12-07 23:30:58 +0000

    feat(components): sidebar design updates

--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -7,7 +7,7 @@ interface SideBarProps {
 export default function SideBar({ children }: SideBarProps) {
   return (
     <div className="absolute h-full w-52 bg-slate-900 px-1 shadow-md">
-      <ul className="relative flex flex-col mt-2 gap-y-2">{children}</ul>
+      <ul className="relative flex flex-col mt-4 gap-y-2">{children}</ul>
     </div>
   );
 }
--- app/components/SideBarRow.tsx
+++ app/components/SideBarRow.tsx
@@ -8,8 +8,8 @@ interface SideBarRowProps {
 export default function SideBarRow({ children, selected }: SideBarRowProps) {
   return (
     <li
-      className={`rounded-md py-1.5 text-center font-medium hover:bg-slate-600 text-white ${
-        selected && 'bg-slate-700'
+      className={`rounded-md py-1.5 text-center font-medium text-white hover:bg-slate-500 hover:bg-opacity-20 ${
+        selected && 'bg-slate-500 bg-opacity-50 hover:bg-opacity-70'
       }`}
     >
       {children}

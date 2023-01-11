4712610b20f64acdb9533733037c6daee6c62348
devnote-dev <inferno0745@gmail.com>
2023-01-11 22:50:26 +0000

    refactor(models): reimpl permission consts & methods

--- app/models/apikey.server.ts
+++ app/models/apikey.server.ts
@@ -5,3 +5,15 @@ export type { ApiKey } from '@prisma/client';
 export function getUserKeys(userId: string) {
   return prisma.apiKey.findMany({ where: { userId } });
 }
+
+export async function createKey(userId: string, permissions: number) {
+  let count = await prisma.apiKey.count({ where: { userId } });
+  if (count >= 10) throw new Error('Limit of 10 API keys reached');
+
+  return prisma.apiKey.create({
+    data: {
+      userId,
+      permissions,
+    },
+  });
+}

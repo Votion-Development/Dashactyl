e268de7fa989ac775e3c29d7aa71eb1eebc05642
devnote-dev <inferno0745@gmail.com>
2023-01-14 21:00:49 +0000

    refactor(dashboard): merge ServerRow into dashboard

--- app/routes/dashboard/index.tsx
+++ app/routes/dashboard/index.tsx
@@ -1,9 +1,9 @@
+import { BsPlugFill } from 'react-icons/bs';
 import type { LoaderArgs, MetaFunction } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 import NavBar from '~/components/NavBar';
 import Progress from '~/components/Progress';
-import ServerRow from '~/components/ServerRow';
 import { getRemoteUserServers } from '~/models/remote.server';
 import { requireUser } from '~/session.server';
 
@@ -20,6 +20,28 @@ export async function loader({ request }: LoaderArgs) {
     servers: data?.[1] || null,
     user,
   });
+}
+
+function getColor(status: string | null): string {
+  switch (status) {
+    case 'suspended':
+      return 'text-red-600';
+    case 'transferring':
+      return 'text-red-400';
+    default:
+      return 'text-white';
+  }
+}
+
+function getStatus(status: string | null): string {
+  switch (status) {
+    case 'suspended':
+      return 'Server is suspended';
+    case 'transferring':
+      return 'Server is transferring';
+    default:
+      return 'Server is working';
+  }
 }
 
 export default function Dashboard() {
@@ -55,11 +77,17 @@ export default function Dashboard() {
         />
       </div>
       <div className="flex justify-center">
-        <div className="mt-6 max-w-lg rounded-md bg-slate-900 p-2 shadow-lg">
+        <div className="mt-6 max-w-xl rounded-md bg-slate-900 p-2 shadow-lg">
           {servers?.length ? (
-            <ol className="gap-y-2">
+            <ol className="flex flex-col gap-y-2">
               {servers.map(s => (
-                <ServerRow {...s} />
+                <li
+                  className="flex justify-center rounded-md bg-slate-800 px-64 py-2"
+                  key={s.id}
+                >
+                  <BsPlugFill className={`h-7 w-7 ${getColor(s.status)}`} />
+                  <p className="text-xl text-white">{s.name}</p>
+                </li>
               ))}
             </ol>
           ) : (

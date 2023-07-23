a22ae8d0cb8f8df0360ef037f8435a239463edfd
devnote-dev <devnote.dev75@gmail.com>
2023-07-23 23:36:56 +0100

    fix(routes): update account api format

--- app/routes/account/api.tsx
+++ app/routes/account/api.tsx
@@ -57,7 +57,7 @@ export default function API() {
   const { keys } = useLoaderData<typeof loader>();
 
   return (
-    <div>
+    <>
       <NavBar account />
       <SideBar>
         <SideBarRow type="link" url="/account">
@@ -70,85 +70,94 @@ export default function API() {
           Two Factor
         </SideBarRow>
       </SideBar>
-      <div className="mt-6 ml-48 grid grid-cols-2 grid-rows-2">
-        <div>
-          <h1 className="ml-4 mb-3 mt-5 text-2xl font-medium text-white">
-            API Keys
-          </h1>
-          <div className="flex max-w-md flex-col justify-center rounded-md bg-slate-800 p-2 shadow-lg">
-            {keys?.length ? (
-              <ol className="flex flex-col gap-y-2">
-                {keys.map(k => (
-                  <li key={k.id}>
-                    <div
-                      className="flex max-w-md flex-row justify-between rounded-md bg-slate-500 p-2 text-white shadow-lg"
-                      onClick={() => navigator.clipboard.writeText(k.id)}
-                    >
-                      <BsFillKeyFill className="h-7 w-7" />
-                      <code className="mt-0.5 font-mono font-bold">{k.id}</code>
-                      <Form method="delete">
-                        <button type="submit">
-                          <input hidden id="id" name="id" defaultValue={k.id} />
-                          <BsTrashFill className="mt-1 h-5 w-5 hover:text-red-400" />
-                        </button>
-                      </Form>
-                    </div>
-                  </li>
-                ))}
-              </ol>
-            ) : (
-              <div className="p-1 text-lg text-white">
-                You have no API keys.
+      <main>
+        <div className="ml-48 mt-6 grid grid-cols-2 grid-rows-2">
+          <div>
+            <h1 className="mb-3 ml-4 mt-5 text-2xl font-medium text-white">
+              API Keys
+            </h1>
+            <div className="flex max-w-md flex-col justify-center rounded-md bg-slate-800 p-2 shadow-lg">
+              {keys?.length ? (
+                <ol className="flex flex-col gap-y-2">
+                  {keys.map(k => (
+                    <li key={k.id}>
+                      <div
+                        className="flex max-w-md flex-row justify-between rounded-md bg-slate-500 p-2 text-white shadow-lg"
+                        onClick={() => navigator.clipboard.writeText(k.id)}
+                      >
+                        <BsFillKeyFill className="h-7 w-7" />
+                        <code className="mt-0.5 font-mono font-bold">
+                          {k.id}
+                        </code>
+                        <Form method="delete">
+                          <button type="submit">
+                            <input
+                              hidden
+                              id="id"
+                              name="id"
+                              defaultValue={k.id}
+                            />
+                            <BsTrashFill className="mt-1 h-5 w-5 hover:text-red-400" />
+                          </button>
+                        </Form>
+                      </div>
+                    </li>
+                  ))}
+                </ol>
+              ) : (
+                <div className="p-1 text-lg text-white">
+                  You have no API keys.
+                </div>
+              )}
+            </div>
+          </div>
+          <div>
+            <h1 className="mb-3 ml-4 mt-5 text-2xl font-medium text-white">
+              Create Key
+            </h1>
+            {data?.errors && (
+              <div
+                className="my-3 flex max-w-sm items-center justify-center rounded-lg bg-red-100 p-2"
+                role="alert"
+              >
+                <div className="flex justify-start text-base font-medium text-red-700">
+                  <BiErrorCircle className="h-6 w-6" />
+                  &nbsp;{data.errors}.
+                </div>
               </div>
             )}
+            <FormBlock method="post">
+              <p className="mb-2 text-sm text-white">
+                Select the permissions below to give the API key.
+              </p>
+              <h3 className="mb-1 text-base font-medium text-white">Servers</h3>
+              <FormCheckBox
+                htmlFor="ServersCreate"
+                id="ServersCreate"
+                text="Create"
+              />
+              <FormCheckBox
+                htmlFor="ServersUpdate"
+                id="ServersUpdate"
+                text="Update"
+              />
+              <FormCheckBox
+                htmlFor="ServersRenew"
+                id="ServersRenew"
+                text="Renew"
+              />
+              <FormCheckBox
+                htmlFor="ServersDelete"
+                id="ServersDelete"
+                text="Delete"
+              />
+              <div className="mt-4">
+                <FormButton text="Create" type="submit" />
+              </div>
+            </FormBlock>
           </div>
         </div>
-        <div>
-          <h1 className="ml-4 mb-3 mt-5 text-2xl font-medium text-white">
-            Create Key
-          </h1>
-          {data?.errors && (
-            <div
-              className="my-3 flex max-w-sm items-center justify-center rounded-lg bg-red-100 p-2"
-              role="alert"
-            >
-              <div className="flex justify-start text-base font-medium text-red-700">
-                <BiErrorCircle className="h-6 w-6" />
-                &nbsp;{data.errors}.
-              </div>
-            </div>
-          )}
-          <FormBlock method="post">
-            <p className="mb-2 text-sm text-white">
-              Select the permissions below to give the API key.
-            </p>
-            <h3 className="mb-1 text-base font-medium text-white">Servers</h3>
-            <FormCheckBox
-              htmlFor="ServersCreate"
-              id="ServersCreate"
-              text="Create"
-            />
-            <FormCheckBox
-              htmlFor="ServersUpdate"
-              id="ServersUpdate"
-              text="Update"
-            />
-            <FormCheckBox
-              htmlFor="ServersRenew"
-              id="ServersRenew"
-              text="Renew"
-            />
-            <FormCheckBox
-              htmlFor="ServersDelete"
-              id="ServersDelete"
-              text="Delete"
-            />
-            <div className="mt-4">
-              <FormButton text="Create" type="submit" />
-            </div>
-          </FormBlock>
-        </div>
-      </div>
-    </div>
+      </main>
+    </>
   );
 }

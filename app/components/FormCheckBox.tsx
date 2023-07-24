ee100661717c1d65ed665c8c7701bfca3282fd6e
devnote-dev <devnote.dev75@gmail.com>
2023-07-24 01:16:16 +0100

    chore(*): formatting

--- app/components/FormCheckBox.tsx
+++ app/components/FormCheckBox.tsx
@@ -14,7 +14,7 @@ export default function FormCheckBox({
   return (
     <div>
       <input
-        className={`${className} float-left mt-1 mr-2 h-4 w-4 cursor-pointer appearance-none rounded-sm border border-gray-300 bg-white bg-contain bg-center bg-no-repeat align-top transition duration-200 checked:border-blue-600 checked:bg-blue-600 focus:outline-none`}
+        className={`${className} float-left mr-2 mt-1 h-4 w-4 cursor-pointer appearance-none rounded-sm border border-gray-300 bg-white bg-contain bg-center bg-no-repeat align-top transition duration-200 checked:border-blue-600 checked:bg-blue-600 focus:outline-none`}
         id={id}
         name={id}
         type="checkbox"

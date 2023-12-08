57e9d6da739dde40dd232c5ba4d71e1c5e668e4e
Jamie_ <59361355+JamieGrimwood@users.noreply.github.com>
2023-12-08 21:21:40 +0000

    Removing all old files

--- app/routes/api/index.ts
+++
@@ -1,9 +0,0 @@
-import { type LoaderArgs, Response } from '@remix-run/node';
-
-export function loader(_: LoaderArgs) {
-  return new Response(JSON.stringify({ version: '3.0.0' }), {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-}
